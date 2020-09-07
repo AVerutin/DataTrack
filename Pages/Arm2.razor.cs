5297f4c3babb4ba506e0bde8c719d4e458e98f24
AlexV <alexbroot@mail.ru>
2020-09-07 16:39:54 +0300

    Add entry-point to ARM2 and ARM3

--- Pages/Arm2.razor.cs
+++ Pages/Arm2.razor.cs
@@ -1,8 +1,31 @@
-﻿
+﻿using System.Threading.Tasks;
 
 namespace DataTrack.Pages
 {
     public partial class Arm2
     {
+        private (ushort key, double value) lastNotification;
+
+        protected override async void OnInitialized()
+        {
+            Notifier.Notify += OnNotify;
+            
+            // Точка входа на страницу
+            
+        }
+        
+        private async Task OnNotify(ushort key, double value)
+        {
+            await InvokeAsync(() =>
+            {
+                lastNotification = (key, value);
+                StateHasChanged();
+            });
+        }
+
+        public void Dispose()
+        {
+            Notifier.Notify -= OnNotify;
+        }
     }
 }
--- Pages/Arm3.razor.cs
+++ Pages/Arm3.razor.cs
@@ -1,7 +1,32 @@
-﻿
+﻿using System.Threading.Tasks;
+
 namespace DataTrack.Pages
 {
     public partial class Arm3
     {
+        private (ushort key, double value) lastNotification;
+
+        protected override async void OnInitialized()
+        {
+            Notifier.Notify += OnNotify;
+            
+            // Точка входа на страницу
+            
+        }
+        
+        private async Task OnNotify(ushort key, double value)
+        {
+            await InvokeAsync(() =>
+            {
+                lastNotification = (key, value);
+                StateHasChanged();
+            });
+        }
+
+        public void Dispose()
+        {
+            Notifier.Notify -= OnNotify;
+        }
+
     }
 }
